f5bb6606f165ba8ecf6c780bd778c818ce986675
karrireddy <karri.reddy@nagarro.com>
2024-07-19 19:10:13 +0530

    add preload none

--- blocks/brand-film-carousel/brand-film-carousel.js
+++ blocks/brand-film-carousel/brand-film-carousel.js
@@ -19,7 +19,7 @@ export default async function decorate(block) {
     const path = videoPathEl?.querySelector('a')?.textContent?.trim();
     videoEl.classList?.add('brand-film__video-container', 'brand-film__video--paused');
     videoEl.innerHTML = `
-      <video class="brand-film__video" src="${publishDomain + path}" poster=${thumbnail} width="100%" playsinline>
+      <video class="brand-film__video" src="${publishDomain + path}" poster=${thumbnail} width="100%" playsinline preload="none">
       </video>
       <span class="brand-film__play-btn"></span>
     `;
