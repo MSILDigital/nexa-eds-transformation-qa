c1d5e38a516aed0a18e40f2a3d2f43f6c8b77a41
karrireddy <karri.reddy@nagarro.com>
2024-07-19 12:57:45 +0530

    sanitize logo

--- blocks/logo/logo.js
+++ blocks/logo/logo.js
@@ -1,7 +1,7 @@
 import utility from '../../utility/utility.js';
 
 export default function decorate(block) {
-  const link = block.querySelector('div a')?.getAttribute('href') || (new URL(window.location.href)).origin;
+  const link = block.querySelector('div a')?.getAttribute('href') || '#';
   const altText = block.querySelector('div:nth-child(2) div')?.textContent?.trim() || 'logo';
   const picture = block.querySelector('div picture');
 
