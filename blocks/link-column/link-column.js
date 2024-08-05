177dcd6d0b6cc6c58e2b403421c65f18066c1e59
karrireddy <karri.reddy@nagarro.com>
2024-08-05 10:52:49 +0530

    footer and header refactor

--- blocks/link-column/link-column.js
+++ blocks/link-column/link-column.js
@@ -1,24 +1,45 @@
-export default function decorate(block) {
-  const [titleEl, ...ctasEl] = block.children;
+import { moveInstrumentation } from '../../scripts/scripts.js';
+import utility from '../../utility/utility.js';
 
-  const title = titleEl?.querySelector('h1, h2, h3, h4, h5, h6') || '';
-  title?.classList?.add('accordian-item');
+export default function decorate(block) {
+  const [titleEl, orientationEl, ...ctasEl] = block.children;
+
+  let titleHtml = '';
+  const heading = titleEl?.querySelector('h1, h2, h3, h4, h5, h6') || '';
+  if (heading) {
+    heading?.classList?.add('accordian-item', 'link-column__heading');
+    titleHtml = heading.outerHTML;
+  } else {
+    const titleText = titleEl?.textContent?.trim();
+    if (titleText) {
+      titleHtml = `
+        <p class="accordiant-item link-column__heading">${titleText}</p>
+      `;
+    }
+  }
+
+  const orientation = orientationEl?.textContent?.trim() || 'link-column-vertical';
 
   let ctaElementsHTML = '';
   if (ctasEl.length > 0) {
     ctaElementsHTML = ctasEl.map((element) => {
-      const [ctaTextEl, linkEl] = element.children;
+      const [ctaTextEl, linkEl, targetEL] = element.children;
       const ctaText = ctaTextEl?.textContent?.trim() || '';
       const link = linkEl?.querySelector('a')?.href || '';
-
-      return `<li><a href="${link}" target="_self" aria-label="${ctaText}">${ctaText}</a></li>`;
+      const target = targetEL?.textContent?.trim() || '_self';
+      const li = document.createElement('li');
+      moveInstrumentation(element, li);
+      li.innerHTML = `<a href="${link}" target="${target}" aria-label="${ctaText}">${ctaText}</a>`;
+      return li.outerHTML;
     }).join('');
   }
 
-  block.innerHTML = `<div class="link-grid-column">
-                           ${title.outerHTML || ''}
-                           <ul class="content links-container">
-                             ${ctaElementsHTML || ''}
-                           </ul>
-                         </div>`;
+  block.innerHTML = utility.sanitizeHtml(
+    `<div class="link-grid-column ${orientation}">
+      ${titleHtml}
+      <ul class="content links-container accordian-content">
+        ${ctaElementsHTML || ''}
+      </ul>
+    </div>`,
+  );
 }
